b315d2f0bb56654208b0d2455386a3e8ef95e7f5
ani9145 <93372159+ani9145@users.noreply.github.com>
2021-11-03 11:41:47 +0530

    ok
    ok

---
+++ signup.js
@@ -0,0 +1,25 @@
+document.querySelector("form").addEventListener("click", signUp);
+ var userArr= JSON.parse(localStorage.getItem("userData")) || []
+
+function signUp(event) {
+    event.preventDefault();
+    var titel = document.getElementById("aa").value;
+    var firstName = document.getElementById("firstname").value;
+    var secoundName = document.getElementById("secoundname").value;
+    var emailAddress = document.getElementById("email_address").value;
+    var mobile = document.getElementById("number").value;
+    var pass = document.getElementById("password").value;
+    var passwordConfirmation = document.getElementById("passwordconfirmation").value;
+var userC={
+   titel: aa,
+   firstName: firstName,
+   secoundName: secoundName,
+   emailAddress: emailAddress,
+   mobile: mobile,
+   pass: pass,
+   passwordConfirmation: passwordConfirmation,
+}
+userArr.push(userC);
+console.log(userArr)
+localStorage.setItem("userData", JSON.stringify(userArr));
+}
