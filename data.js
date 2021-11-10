cafa3d5a96039d167e6dea2aaaccc698c6af766b
Atul <atulkharwal9329@gmail.com>
2021-11-10 16:53:19 +0530

    UPDATE Code
    Updated media queries ,
    DOM functionality,
    form validation.
    Product data file

---
+++ data.js
@@ -0,0 +1,173 @@
+var data = [{
+        id: 1,
+        name: "TRAVEL SIZE DELICATE FACIAL CLEANSER KASHMIRI SAFFRON & NEEM",
+        size: "50ml",
+        category: "Facial Care",
+        categorySlug: "facial-care",
+        categoryBanner: "https://images.forestessentialsindia.com/pub/media/catalog/category/1_Facial_Care_MG_7073_final_2880x640-min-min_1.jpg",
+        thumbnail: "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/314dec89b3219941707ad62ccc90e585/8/8/8882_delicate_facial_cleanser_kashmiri_saffron_neem_50ml_front.png",
+        gallery: [
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8882_delicate_facial_cleanser_kashmiri_saffron_neem_50ml_side.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8882_delicate_facial_cleanser_kashmiri_saffron_neem_50ml_back.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8882_delicate_facial_cleanser_kashmiri_saffron_neem_50ml_front.png"
+        ],
+        price: 350,
+        description: "The secret to proper cleansing is to remove dead cells, toxins and make up, and clear clogged pores, without stripping the skin’s natural oil and moisture content. Following this philosophy, the Forest Essentials Travel Size Delicate Facial Cleanser Kashmiri Saffron & Neem is an ideal gentle and natural alternative to harsh sulphate infused cleansers, and is especially designed to treat sensitive, acne prone and oily skin. This Ayurvedic facial cleanser is infused with purifying herbs including Neem and Kewda to deeply cleanse the skin, while leaving it glowing and balanced after each use.",
+        benefits: [
+            "This Ayurvedic facial cleanser clears clogged pores, while replenishing the skin's natural moisture.",
+            "It purifies and adds natural glow to the complexion.",
+            "It leaves the skin clear, soft and supple.",
+            "Free from chemicals, parabens and petrochemicals.",
+            "Eco-friendly packaging. Travel Friendly."
+        ],
+        reviews: [{
+                username: "Atul",
+                review: "Really a worthy product.",
+                rating: 4.5,
+                date: "HP , Jan 11 , 2021"
+            },
+            {
+                username: "Abhishek",
+                review: "This is so good to use daily...",
+                rating: 5,
+                date: "Lucknow , Mar 21 , 2021"
+            },
+            {
+                username: "Rahul",
+                review: "Nice facewash",
+                rating: 4,
+                date: "UP , Feb 29 , 2021"
+            }
+        ]
+
+    },
+    {
+        id: 2,
+        name: "TRAVEL SIZE DELICATE FACIAL CLEANSER MASHOBRA HONEY, LEMON & ROSEWATER",
+        size: "50ml",
+        category: "Facial Care",
+        categorySlug: "facial-care",
+        categoryBanner: "https://images.forestessentialsindia.com/pub/media/catalog/category/1_Facial_Care_MG_7073_final_2880x640-min-min_1.jpg",
+        thumbnail: "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_front.png",
+        gallery: [
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_front.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_side.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_back.png"
+        ],
+        price: 350,
+        description: "The first rule of Ayurvedic skincare is to cleanse, tone and moisturise. The Forest Essentials Travel Size Delicate Facial Cleanser Mashobra Honey, Lemon & Rosewater contains Lemon essential oil, which is refreshing and uplifting, organic Honey which is a natural humectant and cleansing agent that absorbs impurities from the skin, and steam distilled pure Rose Water is a natural astringent that tones and rehydrates the skin. It is the perfect facial cleanser for normal to dry skin.",
+        benefits: [
+            "This Ayurvedic facial cleanser removes dirt without stripping the skin of moisture.",
+            "It purifies and adds natural glow to the complexion.",
+            "It leaves the skin clear, soft and supple.",
+            "Free from chemicals, parabens and petrochemicals.",
+            "Eco-friendly packaging. Travel Friendly."
+        ],
+        reviews: [{
+                username: "Atul",
+                review: "Really a worthy product.",
+                rating: 4,
+                date: "HP , Jan 11 , 2021"
+            },
+            {
+                username: "Abhishek",
+                review: "Average Product For me",
+                rating: 3,
+                date: "Lucknow , Mar 21 , 2021"
+            },
+            {
+                username: "Rahul",
+                review: "Nice facewash",
+                rating: 5,
+                date: "UP , Feb 29 , 2021"
+            }
+        ]
+
+    },
+    {
+        id: 3,
+        name: "TRAVEL SIZE DELICATE FACIAL CLEANSER MASHOBRA HONEY, LEMON & ROSEWATER",
+        size: "50ml",
+        category: "Body Care",
+        categorySlug: "body-care",
+        categoryBanner: "https://images.forestessentialsindia.com/pub/media/catalog/category/2_Body_Care_MG_7103_final_2880x640-min-min.jpg",
+        thumbnail: "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_front.png",
+        gallery: [
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_front.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_side.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_back.png"
+        ],
+        price: 350,
+        description: "The first rule of Ayurvedic skincare is to cleanse, tone and moisturise. The Forest Essentials Travel Size Delicate Facial Cleanser Mashobra Honey, Lemon & Rosewater contains Lemon essential oil, which is refreshing and uplifting, organic Honey which is a natural humectant and cleansing agent that absorbs impurities from the skin, and steam distilled pure Rose Water is a natural astringent that tones and rehydrates the skin. It is the perfect facial cleanser for normal to dry skin.",
+        benefits: [
+            "This Ayurvedic facial cleanser removes dirt without stripping the skin of moisture.",
+            "It purifies and adds natural glow to the complexion.",
+            "It leaves the skin clear, soft and supple.",
+            "Free from chemicals, parabens and petrochemicals.",
+            "Eco-friendly packaging. Travel Friendly."
+        ],
+        reviews: [{
+                username: "Atul",
+                review: "Really a worthy product.",
+                rating: 4,
+                date: "HP , Jan 11 , 2021"
+            },
+            {
+                username: "Abhishek",
+                review: "Average Product For me",
+                rating: 3,
+                date: "Lucknow , Mar 21 , 2021"
+            },
+            {
+                username: "Rahul",
+                review: "Nice facewash",
+                rating: 5,
+                date: "UP , Feb 29 , 2021"
+            }
+        ]
+
+    },
+    {
+        id: 4,
+        name: "TRAVEL SIZE DELICATE FACIAL CLEANSER MASHOBRA HONEY, LEMON & ROSEWATER",
+        size: "50ml",
+        category: "Body Care",
+        categorySlug: "body-care",
+        categoryBanner: "https://images.forestessentialsindia.com/pub/media/catalog/category/2_Body_Care_MG_7103_final_2880x640-min-min.jpg",
+        thumbnail: "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_front.png",
+        gallery: [
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_front.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_side.png",
+            "https://images.forestessentialsindia.com/pub/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/8/8/8884_delicate_facial_cleanser_mashobra_honey_lemon_rosewater_50ml_back.png"
+        ],
+        price: 350,
+        description: "The first rule of Ayurvedic skincare is to cleanse, tone and moisturise. The Forest Essentials Travel Size Delicate Facial Cleanser Mashobra Honey, Lemon & Rosewater contains Lemon essential oil, which is refreshing and uplifting, organic Honey which is a natural humectant and cleansing agent that absorbs impurities from the skin, and steam distilled pure Rose Water is a natural astringent that tones and rehydrates the skin. It is the perfect facial cleanser for normal to dry skin.",
+        benefits: [
+            "This Ayurvedic facial cleanser removes dirt without stripping the skin of moisture.",
+            "It purifies and adds natural glow to the complexion.",
+            "It leaves the skin clear, soft and supple.",
+            "Free from chemicals, parabens and petrochemicals.",
+            "Eco-friendly packaging. Travel Friendly."
+        ],
+        reviews: [{
+                username: "Atul",
+                review: "Really a worthy product.",
+                rating: 4,
+                date: "HP , Jan 11 , 2021"
+            },
+            {
+                username: "Abhishek",
+                review: "Average Product For me",
+                rating: 3,
+                date: "Lucknow , Mar 21 , 2021"
+            },
+            {
+                username: "Rahul",
+                review: "Nice facewash",
+                rating: 5,
+                date: "UP , Feb 29 , 2021"
+            }
+        ]
+
+    },
+]
